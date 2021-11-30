e82aacbff4d71a96daf57bd9b7ccbefb6eb626cf
Alexey B.V <ablogmount@gmail.com>
2021-11-30 18:55:03 +0500

    Express Generator REST API

---
+++ routes/leadersRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leadersRouter = express.Router();
+
+leadersRouter.use(bodyParser.json());
+
+leadersRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders');
+})
+
+leadersRouter.route('/:leadersId')
+.get((req,res,next) => {
+    res.end('Will send details of the leader: ' + req.params.leadersId +' to you!');
+})
+.post((req, res, next) => {
+  res.end('POST operation not supported on /leaders/'+ req.params.leadersId);
+})
+.put((req, res, next) => {
+  res.write('Updating the leader: ' + req.params.leadersId + '\n');
+  res.end('Will update the leader: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leadersId);
+});
+
+module.exports = leadersRouter;
